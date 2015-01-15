ceb3517b4f3afcb75010acd7ce424953307507c6
Ivaylo1987 <ivaylo1987@gmail.com>
2015-01-15 17:33:40 +0200

    sorting refactoring

--- HoundRace.Web/Controllers/HomeController.cs
+++ HoundRace.Web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@
     using System;
     using System.Web.Mvc;
     using System.Linq;
-using System.Collections.Generic;
+    using System.Collections.Generic;
 
     public class HomeController : Controller
     {
@@ -33,7 +33,19 @@ using System.Collections.Generic;
 
         public ActionResult RefreshRaceEvents(int eventNumber)
         {
-            var raceEvent = this.GetRaceEvents().FirstOrDefault( r => r.EventNumber == eventNumber);
+            var raceEvent = this.GetRaceEvents().FirstOrDefault(r => r.EventNumber == eventNumber);
+
+            if (Request.QueryString["byName"] != null)
+            {
+                var nameValue = Request.QueryString["byName"];
+                raceEvent = this.OrderByValue(e => e.Name, nameValue, raceEvent);
+            }
+
+            if (Request.QueryString["byOdds"] != null)
+            {
+                var oddsValue = Request.QueryString["byOdds"];
+                raceEvent = this.OrderByValue(e => e.OddsDecimal, oddsValue, raceEvent);
+            }
 
             return this.PartialView("RaceEventPartial", raceEvent);
         }
@@ -43,7 +55,26 @@ using System.Collections.Generic;
             var filePath = this.Server.MapPath("~/App_Data/race.xml");
             var raceEvents = this.raceEventService.GetRaceEvents(filePath).Select(RaceEventViewModel.FromRaceEvent).ToArray();
 
+            for (int i = 0; i < raceEvents.Count(); i++)
+            {
+                raceEvents[i].Entries = raceEvents[i].Entries.OrderBy(e => e.Name);
+            }
+
             return raceEvents;
+        }
+
+        private RaceEventViewModel OrderByValue<T>(Func<EntryViewModel, T> orderExpression, string value, RaceEventViewModel raceEvent)
+        {
+            if (value == "Ascending")
+            {
+                raceEvent.Entries = raceEvent.Entries.OrderBy(orderExpression);
+            }
+            else
+            {
+                raceEvent.Entries = raceEvent.Entries.OrderByDescending(orderExpression);
+            }
+
+            return raceEvent;
         }
     }
 }
--- HoundRace.Web/Scripts/ajaxPolling.js
+++ HoundRace.Web/Scripts/ajaxPolling.js
@@ -1,9 +1,33 @@
 ﻿(function () {
+
+    (function changeSortingParams() {
+        $(".btn-sort").on("click", function () {
+            var filterBtn = $(this);
+            var refreshBtn = filterBtn.siblings(".refresh").first();
+            var refreshLink = refreshBtn.attr("href");
+            var filerValue;
+
+            if (filterBtn.attr("value") === "Ascending") {
+                filterBtn.attr("value", "Descending");
+                filerValue = "Descending";
+            } else {
+                filterBtn.attr("value", "Ascending");
+                filerValue = "Ascending";
+            }
+
+            refreshLink = refreshLink.split("&")[0];
+            refreshLink += "&" + filterBtn.attr("name") + "=" + filerValue;
+            refreshBtn.attr("href", refreshLink);
+
+            
+        })
+    })();
+
     (function poll() {
         setTimeout(function () {
             $(".refresh").click();
             console.log("Clicked!")
             poll();
-        }, 2000);
+        }, 1000);
     })();
 })()
